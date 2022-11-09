b60817a2126fde9802dd701687146bbecc13f36d
Eugene_Bobikov <mr.bobikov@inbox.ru>
2022-11-09 21:49:16 +0300

    add useEffect hook

--- first-project/first-project-react/src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ first-project/first-project-react/src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 // import s from "./ProfileInfo.module.css";
 
 const ProfileStatusWithHooks = (props) => {
   const [editMode, setEditMode] = useState(false);
   const [status, setStatus] = useState(props.status);
+
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
 
   const activatedEditMode = () => {
     setEditMode(true);
